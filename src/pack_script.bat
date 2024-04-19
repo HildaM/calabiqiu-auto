e3df5b00a5e6e3ce7454fda402e15b9e2b2ad107
freshman2233 <freshmanfreshman0011@gmail.com>
2024-04-20 00:10:17 +0800

    debugger

--- src/pack_script.bat
+++ src/pack_script.bat
@@ -1,4 +1,4 @@
 @echo off
-python -m PyInstaller --onefile --add-data="../images;images" --windowed .\main.py
+python -m PyInstaller --onefile --add-data="images;images" --windowed .\main.py
 pause
 pause
